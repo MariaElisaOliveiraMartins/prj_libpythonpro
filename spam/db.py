b1c4f9a7bc3475c1e77fc830f25d8855786ade09
Maria Elisa Oliveira Martins <mariaelisaoliveiramartins@gmail.com>
2020-02-14 21:31:57 -0200

    Camada de persistência do projeto de envio de e-mail

---
+++ spam/db.py
@@ -0,0 +1,28 @@
+class Sessao:
+
+    contador = 0
+
+    lista_usr = []
+
+    def salvar(self, usuario):
+        Sessao.contador += 1
+        usuario.id = Sessao.contador
+        self.lista_usr.append(usuario)
+
+    def rollback(self):
+        pass
+
+    def fechar(self):
+        pass
+
+    def listar(self):
+        return Sessao.lista_usr
+
+
+class Conexao:
+
+    def gerar_sessao(self):
+        return Sessao()
+
+    def fechar(self):
+        pass
