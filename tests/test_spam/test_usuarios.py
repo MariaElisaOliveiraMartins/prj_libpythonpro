f8f77ab037986f8aa6580570b5d4426db3259a1d
Maria Elisa Oliveira Martins <mariaelisaoliveiramartins@gmail.com>
2020-02-15 00:40:55 -0200

    A espera de uma solução - rever video

--- tests/test_spam/test_usuarios.py
+++ tests/test_spam/test_usuarios.py
@@ -1,25 +1,34 @@
+import pytest
 from prj_libpythonpro.spam.db import Conexao
+#from prj_libpythonpro.spam.db import Sessao
 from prj_libpythonpro.spam.modelos import Usuario
 
 
+@pytest.fixture
+def sessao():
+    sessao_obj = conexao.gerar.sessao()
+    yield sessao_obj
+    sessao_obj.rollback()
+    sessao_obj.fechar()
+
+
+@pytest.fixture
+def conexao():
+    #setup
+    conexao_obj = Conexao()
+    yield conexao_obj
+    #Tear Down
+    conexao_obj.fechar()
+
+
 def test_salvar_usuarios():
-    conexao = Conexao()
-    sessao = conexao.gerar_sessao()
     usuario = Usuario(nome='Maria Elisa')
     sessao.salvar(usuario)
     assert isinstance(usuario.id, int)
-    sessao.rollback()
-    sessao.fechar()
-    conexao.fechar()
 
 
 def test_listar_usuarios():
-    conexao = Conexao()
-    sessao = conexao.gerar_sessao()
     usuarios = [Usuario(nome='Nome do Usuário 01'), Usuario(nome='Nome do Usuário 02')]
     for usuario in usuarios:
         sessao.salvar(usuario)
     assert usuarios == sessao.listar()
-    sessao.rollback()
-    sessao.fechar()
-    conexao.fechar()
