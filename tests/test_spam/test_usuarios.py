f7f84d5feb62ff3307ae49495f734468a52dc21d
Maria Elisa Oliveira Martins <mariaelisaoliveiramartins@gmail.com>
2020-02-15 17:49:15 -0200

    Correção de código (inclusão do parametro "sessao" nas funções de teste da camada de persistencia)

--- tests/test_spam/test_usuarios.py
+++ tests/test_spam/test_usuarios.py
@@ -1,23 +1,4 @@
-import pytest
-from prj_libpythonpro.spam.db import Conexao
 from prj_libpythonpro.spam.modelos import Usuario
-
-
-@pytest.fixture
-def conexao():
-    #setup
-    conexao_obj = Conexao()
-    yield conexao_obj
-    #Tear Down
-    conexao_obj.fechar()
-
-
-@pytest.fixture
-def sessao(conexao):
-    sessao_obj = conexao.gerar_sessao()
-    yield sessao_obj
-    sessao_obj.rollback()
-    sessao_obj.fechar()
 
 
 def test_salvar_usuarios(sessao):
