c91432158270c905e060fce260516d335eac5dc1
Maria Elisa Oliveira Martins <mariaelisaoliveiramartins@gmail.com>
2020-02-15 23:10:27 -0300

    Inclusão do endereço de e-mail do usuário no teste do banco de dados.

--- tests/test_spam/test_usuarios.py
+++ tests/test_spam/test_usuarios.py
@@ -2,13 +2,14 @@ from prj_libpythonpro.spam.modelos import Usuario
 
 
 def test_salvar_usuarios(sessao):
-    usuario = Usuario(nome='Maria Elisa')
+    usuario = Usuario(nome='Maria Elisa', email='mariaelisaoliveiramartins@gmail.com')
     sessao.salvar(usuario)
     assert isinstance(usuario.id, int)
 
 
 def test_listar_usuarios(sessao):
-    usuarios = [Usuario(nome='Nome do Usuário 01'), Usuario(nome='Nome do Usuário 02')]
+    usuarios = [Usuario(nome='Nome do Usuário 01', email='MEOM@gmail.com'),
+                Usuario(nome='Nome do Usuário 02', email='OMFCO@gmail.com')]
     for usuario in usuarios:
         sessao.salvar(usuario)
     assert usuarios == sessao.listar()
