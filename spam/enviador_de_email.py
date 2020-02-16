aa0e2e9c39cc265bd42dd7c4700604be73fcd126
Maria Elisa Oliveira Martins <mariaelisaoliveiramartins@gmail.com>
2020-02-15 23:50:11 -0200

    Inclusão do contador de e-mails enviados para destinatários válidos.

--- spam/enviador_de_email.py
+++ spam/enviador_de_email.py
@@ -1,7 +1,11 @@
 class Enviador():
+    def __init__(self):
+        self.qtde_emails_env = 0
+
     def enviar(self, remetente, destinatario, assunto, mensagem):
         if '@' not in destinatario:
             raise EmailInvalido (f' Destinatário Invalido : {destinatario} ')
+        self.qtde_emails_env += 1
         return destinatario
 
 
