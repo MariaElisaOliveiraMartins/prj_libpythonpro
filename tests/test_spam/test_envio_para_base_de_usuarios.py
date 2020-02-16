3ea796b1cd550095a037244454deca5c635531e5
Maria Elisa Oliveira Martins <mariaelisaoliveiramartins@gmail.com>
2020-02-16 00:30:48 -0300

    Cod. Intermediário passível de correção:
    --> Implementações de testes utilizando Mock.

--- tests/test_spam/test_envio_para_base_de_usuarios.py
+++ tests/test_spam/test_envio_para_base_de_usuarios.py
@@ -18,8 +18,29 @@ from prj_libpythonpro.spam.modelos import Usuario
 )
 
 
+class EnviadorMock(Enviador):
+
+    def __init__(self):
+        super().__init__()
+        self.parametros_de_envios = None
+        self.qtde_emails_env = 0
+
+    def enviar(self, remetente, destinatario, assunto, mensagem):
+        self.qtde_emails_env += 1
+
+
 def test_qtde_de_spam(sessao, usuarios):
     for usrs in usuarios:
         sessao.salvar(usrs)
-    enviador_de_spam = EnviadorDeSpam(sessao, Enviador())
-    enviador_de_spam.enviar_emails('remetente@gmail.com', 'Assunto: titulo do e-mail', 'Msg: blá-blá-blá-blá')
+    enviador_de_spam = EnviadorDeSpam(sessao, EnviadorMock())
+    enviador_de_spam.enviar_emails('remetente_1@gmail.com', 'Assunto: titulo do e-mail 01', 'Msg: 01 - blá-blá-blá-blá')
+
+
+def test_parametros_de_spam(sessao):
+    usuario = Usuario(nome='Maria Elisa', email='mariaelisa@gmail.com')
+    sessao.salvar(usuario)
+    enviador = EnviadorMock()
+    enviador_de_spam = EnviadorDeSpam(sessao, enviador)
+    enviador_de_spam.enviar_emails('remetente_2@gmail.com', 'Assunto: titulo do e-mail 02', 'Msg: 02 - blá-blá-blá-blá')
+    assert enviador.parametros_de_envio == ('meom@gmail.com', 'mariaelisa@gmail.com',
+                                            'Assunto: titulo do e-mail 02', 'Msg: 02 - blá-blá-blá-blá')
