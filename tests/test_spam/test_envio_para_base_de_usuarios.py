1ae76fe8c07a878b2529f176fc540b7e1f2798d4
Maria Elisa Oliveira Martins <mariaelisaoliveiramartins@gmail.com>
2020-02-15 23:11:52 -0300

    Implementação do teste de spam com contagem da quantidade de e-mails enviados.

--- tests/test_spam/test_envio_para_base_de_usuarios.py
+++ tests/test_spam/test_envio_para_base_de_usuarios.py
@@ -1,7 +1,25 @@
+import pytest
 from prj_libpythonpro.spam.enviador_de_email import Enviador
 from prj_libpythonpro.spam.main import EnviadorDeSpam
+from prj_libpythonpro.spam.modelos import Usuario
 
 
-def test_envio_de_spam(sessao):
+@pytest.mark.parametrize(
+    'usuarios',
+    [
+        [
+            Usuario(nome='Nome do Usuário 01', email='MEOM@gmail.com'),
+            Usuario(nome='Nome do Usuário 02', email='OMFCO@gmail.com')
+        ],
+        [
+            Usuario(nome='Nome do Usuário 01', email='mariaelisa@gmail.com')
+        ]
+    ]
+)
+
+
+def test_qtde_de_spam(sessao, usuarios):
+    for usrs in usuarios:
+        sessao.salvar(usrs)
     enviador_de_spam = EnviadorDeSpam(sessao, Enviador())
     enviador_de_spam.enviar_emails('remetente@gmail.com', 'Assunto: titulo do e-mail', 'Msg: blá-blá-blá-blá')
