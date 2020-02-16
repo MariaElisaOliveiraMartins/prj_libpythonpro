83745929a51de68aec4a9ab9a840e11e78eaaa22
Maria Elisa Oliveira Martins <mariaelisaoliveiramartins@gmail.com>
2020-02-16 00:28:50 -0300

    Retorno ao codigo original, isso é, sem implementações de testes, devido a utilização do Mock.

--- spam/enviador_de_email.py
+++ spam/enviador_de_email.py
@@ -1,13 +1,10 @@
 class Enviador():
-    def __init__(self):
-        self.qtde_emails_env = 0
 
     def enviar(self, remetente, destinatario, assunto, mensagem):
         if '@' not in destinatario:
             raise EmailInvalido (f' Destinatário Invalido : {destinatario} ')
-        self.qtde_emails_env += 1
-        return destinatario
 
+        return destinatario
 
 
 class EmailInvalido(Exception):
