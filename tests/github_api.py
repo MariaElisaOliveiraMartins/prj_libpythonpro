3ba7d125c0f12db7e3a0ae0e454736ff6c2fc002
Maria Elisa Oliveira Martins <mariaelisaoliveiramartins@gmail.com>
2020-02-17 19:49:03 -0300

    Inibição, com comentário, da função main para efeito de substituição pele test da função "buscar_avatar".
    Obs: inclusão do retorno da função "resp.json" no comentário para efeito de elucidar o exemplo, posteriormente, se necessário.

--- tests/github_api.py
+++ tests/github_api.py
@@ -21,4 +21,13 @@ Substituição desse trecho do código pelo Mock
 if __name__ == '__main__':
     print(buscar_avatar('MariaElisaOliveiraMartins'))
 
+obs: copia do valor de retorno do resp.json:
+
+{"login":"MariaElisaOliveiraMartins",
+"id":58866392,"node_id":"MDQ6VXNlcjU4ODY2Mzky",
+"avatar_url":"https://avatars1.githubusercontent.com/u/58866392?v=4",
+"gravatar_id":"",
+"url":"https://api.github.com/users/MariaElisaOliveiraMartins",
+...}
+
 '''
