088d2eab4c365cb34ac5d951dd12f885a064943b
Maria Elisa Oliveira Martins <mariaelisaoliveiramartins@gmail.com>
2020-02-17 19:53:25 -0300

    Inclusão do Mock no teste do módulo github_api.py para obter o retorno da função "buscar_avatar" simulando a resposta do servidor Github. (Great!)

--- tests/test_spam/test_github_api.py
+++ tests/test_spam/test_github_api.py
@@ -1,5 +1,12 @@
+from unittest.mock import Mock
 from prj_libpythonpro.tests import github_api
 
 def test_buscar_avatar():
+    resp_mock = Mock()
+    resp_mock.json.return_value = {"login":"MariaElisaOliveiraMartins",
+                                   "id":58866392,"node_id":"MDQ6VXNlcjU4ODY2Mzky",
+                                   "avatar_url":"https://avatars1.githubusercontent.com/u/58866392?v=4"
+                                  }
+    github_api.requests.get = Mock(return_value=resp_mock)
     url = github_api.buscar_avatar('MariaElisaOliveiraMartins')
     assert 'https://avatars1.githubusercontent.com/u/58866392?v=4' == url
