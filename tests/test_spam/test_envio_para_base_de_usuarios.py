157d0931b5160179598776c127d86cb1adb208c5
Maria Elisa Oliveira Martins <mariaelisaoliveiramartins@gmail.com>
2020-02-16 22:46:59 -0300

    Substituição do teste de envio de spam pelas funções do Mock.

--- tests/test_spam/test_envio_para_base_de_usuarios.py
+++ tests/test_spam/test_envio_para_base_de_usuarios.py
@@ -1,8 +1,11 @@
 import pytest
-from prj_libpythonpro.spam.enviador_de_email import Enviador
+from unittest.mock import Mock
 from prj_libpythonpro.spam.main import EnviadorDeSpam
 from prj_libpythonpro.spam.modelos import Usuario
 
+'''
+
+Essr trecho do código foi substituido pelo Mock:
 
 class EnviadorMock(Enviador):
 
@@ -14,6 +17,8 @@ class EnviadorMock(Enviador):
     def enviar(self, remetente, destinatario, assunto, mensagem):
         self.parametros_de_envios = (remetente, destinatario, assunto, mensagem)
         self.qtde_emails_env += 1
+
+'''
 
 
 @pytest.mark.parametrize(
@@ -33,16 +38,18 @@ class EnviadorMock(Enviador):
 def test_qtde_de_spam(sessao, lst_usuarios):
     for user in lst_usuarios:
         sessao.salvar(user)
-    enviador_de_spam = EnviadorDeSpam(sessao, EnviadorMock())
+    enviador = Mock()
+    enviador_de_spam = EnviadorDeSpam(sessao, enviador)
     enviador_de_spam.enviar_emails('remetente_1@gmail.com', 'Assunto: titulo do e-mail 01', 'Msg: 01 - blá-blá-blá-blá')
+    contador = enviador.enviar.call_count
+    assert len(lst_usuarios) == contador
 
 
 def test_parametros_de_spam(sessao):
-#Usuario(nome='Nome do Usuário 01', email='mariaelisa@gmail.com')
     user = Usuario(nome='Maria Elisa', email='mariaelisa@gmail.com')
     sessao.salvar(user)
-    enviador = EnviadorMock()
+    enviador = Mock()
     enviador_de_spam = EnviadorDeSpam(sessao, enviador)
     enviador_de_spam.enviar_emails('remetente_2@gmail.com', 'Assunto: titulo do e-mail 02', 'Msg: 02 - blá-blá-blá-blá')
-    assert enviador.parametros_de_envios == ('remetente_2@gmail.com', 'mariaelisa@gmail.com',
-                                            'Assunto: titulo do e-mail 02', 'Msg: 02 - blá-blá-blá-blá')
+    enviador.enviar.assert_called_once_with('remetente_2@gmail.com', 'mariaelisa@gmail.com',
+                                          'Assunto: titulo do e-mail 02', 'Msg: 02 - blá-blá-blá-blá')
