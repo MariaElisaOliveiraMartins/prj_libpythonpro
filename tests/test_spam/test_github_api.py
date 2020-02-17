4f161b1881474fe39c2c9153861f88c7ca0e4a74
Maria Elisa Oliveira Martins <mariaelisaoliveiramartins@gmail.com>
2020-02-17 20:30:18 -0300

    Inclusão da fixture de teardown para eliminar a interferencia do teste unitário (unittest) no teste integrado (pytest).

--- tests/test_spam/test_github_api.py
+++ tests/test_spam/test_github_api.py
@@ -1,12 +1,30 @@
+import pytest
 from unittest.mock import Mock
 from prj_libpythonpro.tests import github_api
 
-def test_buscar_avatar():
+
+@pytest.fixture
+def avatar_url():
     resp_mock = Mock()
-    resp_mock.json.return_value = {"login":"MariaElisaOliveiraMartins",
-                                   "id":58866392,"node_id":"MDQ6VXNlcjU4ODY2Mzky",
-                                   "avatar_url":"https://avatars1.githubusercontent.com/u/58866392?v=4"
-                                  }
+    url = "https://avatars1.githubusercontent.com/u/58866392?v=4"
+    resp_mock.json.return_value = {"login": "MariaElisaOliveiraMartins",
+                                   "id": 58866392, "node_id": "MDQ6VXNlcjU4ODY2Mzky",
+                                   "avatar_url": url
+                                   }
+    get_original = github_api.requests.get
     github_api.requests.get = Mock(return_value=resp_mock)
+    yield url
+    github_api.requests.get = get_original
+
+
+
+def test_buscar_avatar(avatar_url): #teste unitário (isolado no ambiente local)
+    url = github_api.buscar_avatar('MariaElisaOliveiraMartins')
+    assert avatar_url == url
+
+
+
+def test_integrado_buscar_avatar(): #teste integrado (acesso ao ambiente externo)
+
     url = github_api.buscar_avatar('MariaElisaOliveiraMartins')
     assert 'https://avatars1.githubusercontent.com/u/58866392?v=4' == url
