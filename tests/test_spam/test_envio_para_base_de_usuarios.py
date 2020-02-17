5939c739e3c6eca02664140991ef9de1fcd02a43
Maria Elisa Oliveira Martins <mariaelisaoliveiramartins@gmail.com>
2020-02-16 21:38:00 -0300

    Correção do código (lógica)

--- tests/test_spam/test_envio_para_base_de_usuarios.py
+++ tests/test_spam/test_envio_para_base_de_usuarios.py
@@ -4,8 +4,20 @@ from prj_libpythonpro.spam.main import EnviadorDeSpam
 from prj_libpythonpro.spam.modelos import Usuario
 
 
+class EnviadorMock(Enviador):
+
+    def __init__(self):
+        super().__init__()
+        self.parametros_de_envios = None
+        self.qtde_emails_env = 0
+
+    def enviar(self, remetente, destinatario, assunto, mensagem):
+        self.parametros_de_envios = (remetente, destinatario, assunto, mensagem)
+        self.qtde_emails_env += 1
+
+
 @pytest.mark.parametrize(
-    'usuarios',
+    "lst_usuarios",
     [
         [
             Usuario(nome='Nome do Usuário 01', email='MEOM@gmail.com'),
@@ -18,29 +30,19 @@ from prj_libpythonpro.spam.modelos import Usuario
 )
 
 
-class EnviadorMock(Enviador):
-
-    def __init__(self):
-        super().__init__()
-        self.parametros_de_envios = None
-        self.qtde_emails_env = 0
-
-    def enviar(self, remetente, destinatario, assunto, mensagem):
-        self.qtde_emails_env += 1
-
-
-def test_qtde_de_spam(sessao, usuarios):
-    for usrs in usuarios:
-        sessao.salvar(usrs)
+def test_qtde_de_spam(sessao, lst_usuarios):
+    for user in lst_usuarios:
+        sessao.salvar(user)
     enviador_de_spam = EnviadorDeSpam(sessao, EnviadorMock())
     enviador_de_spam.enviar_emails('remetente_1@gmail.com', 'Assunto: titulo do e-mail 01', 'Msg: 01 - blá-blá-blá-blá')
 
 
 def test_parametros_de_spam(sessao):
-    usuario = Usuario(nome='Maria Elisa', email='mariaelisa@gmail.com')
-    sessao.salvar(usuario)
+#Usuario(nome='Nome do Usuário 01', email='mariaelisa@gmail.com')
+    user = Usuario(nome='Maria Elisa', email='mariaelisa@gmail.com')
+    sessao.salvar(user)
     enviador = EnviadorMock()
     enviador_de_spam = EnviadorDeSpam(sessao, enviador)
     enviador_de_spam.enviar_emails('remetente_2@gmail.com', 'Assunto: titulo do e-mail 02', 'Msg: 02 - blá-blá-blá-blá')
-    assert enviador.parametros_de_envio == ('meom@gmail.com', 'mariaelisa@gmail.com',
+    assert enviador.parametros_de_envios == ('remetente_2@gmail.com', 'mariaelisa@gmail.com',
                                             'Assunto: titulo do e-mail 02', 'Msg: 02 - blá-blá-blá-blá')
