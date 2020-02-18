8ed0352af21895cc551179cbfcc602ea1d9258a0
Maria Elisa Oliveira Martins <mariaelisaoliveiramartins@gmail.com>
2020-02-18 20:04:37 -0300

    Correção de erros apontados pelo flake8 (PEP-8).

--- spam/enviador_de_email.py
+++ spam/enviador_de_email.py
@@ -2,7 +2,7 @@ class Enviador():
 
     def enviar(self, remetente, destinatario, assunto, mensagem):
         if '@' not in destinatario:
-            raise EmailInvalido (f' Destinatário Invalido : {destinatario} ')
+            raise EmailInvalido(f' Destinatário Invalido : {destinatario} ')
         return destinatario
 
 
