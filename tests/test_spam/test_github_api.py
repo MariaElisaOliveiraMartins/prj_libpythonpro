99fba5ae8644b83bfd766e6d5fe2f2c4e1671c37
Maria Elisa Oliveira Martins <mariaelisaoliveiramartins@gmail.com>
2020-02-17 21:47:57 -0300

    Alteração da fixture de teardown para utilizar o plugin pytest-mock.

--- tests/test_spam/test_github_api.py
+++ tests/test_spam/test_github_api.py
@@ -4,18 +4,16 @@ from prj_libpythonpro.tests import github_api
 
 
 @pytest.fixture
-def avatar_url():
+def avatar_url(mocker):
     resp_mock = Mock()
     url = "https://avatars1.githubusercontent.com/u/58866392?v=4"
     resp_mock.json.return_value = {"login": "MariaElisaOliveiraMartins",
                                    "id": 58866392, "node_id": "MDQ6VXNlcjU4ODY2Mzky",
                                    "avatar_url": url
                                    }
-    get_original = github_api.requests.get
-    github_api.requests.get = Mock(return_value=resp_mock)
-    yield url
-    github_api.requests.get = get_original
-
+    get_mock = mocker.patch('prj_libpythonpro.tests.github_api.requests.get')
+    get_mock.return_value = resp_mock
+    return url
 
 
 def test_buscar_avatar(avatar_url): #teste unitário (isolado no ambiente local)
